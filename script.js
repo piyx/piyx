d3fbd2a5141bf59b9684c76c2c73c6c5f14309dc
piyx <piyx.abhishek@gmail.com>
2021-09-15 02:07:48 +0530

    add data on scroll animations

---
+++ script.js
@@ -0,0 +1,24 @@
+let animationSpeed = "700"
+let contentAnimationType = "fade-up"
+let headingAnimationType = "fade-right"
+
+cards = document.getElementsByClassName('card')
+headers = document.getElementsByClassName('header')
+projects = document.getElementsByClassName('column')
+summary = document.getElementsByClassName('summary')
+workexp = document.getElementsByClassName('workexplist')
+socials = document.getElementsByClassName('socials')
+navbar = document.getElementsByTagName('nav')
+
+elements = [...cards, ...projects, ...summary, ...workexp, ...socials, ...navbar]
+Array.from(elements).forEach(element => {
+    element.setAttribute("data-aos", contentAnimationType)
+    element.setAttribute("data-aos-duration", animationSpeed)
+})
+
+Array.from(headers).forEach(element => {
+    element.setAttribute("data-aos", headingAnimationType)
+    element.setAttribute("data-aos-duration", animationSpeed)
+})
+
+AOS.init()
